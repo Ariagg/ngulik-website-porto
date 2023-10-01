c512818b971a94efd78732d54f7e3a9159ddfc3b
7k <19211210@bsi.ac.id>
2023-10-01 13:04:57 +0700

    update animasi hamburger kece
    update liblary hamburgers done

--- style/script.js
+++ style/script.js
@@ -1,6 +1,19 @@
-const toggleButton = document.getElementsByClassName('hamburger')[0]
-const navbarLink = document.getElementsByClassName('navbar-link')[0]
+// const toggleButton = document.getElementsByClassName('hamburger--collapse')[0]
+// const navbarLink = document.getElementsByClassName('navbar-link')[0]
 
-toggleButton.addEventListener('click', () => {
-    navbarLink.classList.toggle('active')
-})
+// toggleButton.addEventListener('click', () => {
+//     navbarLink.classList.toggle('active')
+// })
+
+  // Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+var navbarLink = document.getElementsByClassName('navbar-link')[0]
+
+ // On click
+hamburger.addEventListener("click", function() {
+// Toggle class "is-active"
+    navbarLink.classList.toggle('active');
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+
+});
